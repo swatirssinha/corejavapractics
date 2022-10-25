ff9cd2bbc483f262ba9180868a668ac23badbfe5
swatirssinha <115774107+swatirssinha@users.noreply.github.com>
2022-10-25 12:42:52 +0530

    Add files via upload

---
+++ 14oct/DataTypesDemo.java
@@ -0,0 +1,17 @@
+class DataTypeDemo
+{
+	//main method
+	public static void main(String args[])
+	{
+		//declaring local variables
+		int number1= 56;
+		int number2= 22;
+
+		//calculating the sum
+		int sum = number1+ number2;
+		System.out.println("The sum is :"+sum); // printing the value of sum
+
+
+	}
+	// end of main
+}
